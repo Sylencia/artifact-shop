02af905b9eed0e5a2052b5e4e603ab148678f0a2
Joseph Tran <joseph.tran@akqa.com>
2018-10-11 10:02:31 +1100

    Linted files Added a  couple of optimisations

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import uniq from 'lodash/uniq'
+import uniqWith from 'lodash/uniqWith'
+import isEqual from 'lodash/isEqual'
+import max from 'lodash/max'
+
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+  componentDidMount() {
+    const consumables = [3, 3, 4, 5, 7, 10]
+    const armor = [3, 6, 6, 7, 8, 8, 9, 13, 15, 16, 19, 19, 25]
+    const weapons = [3, 6, 7, 7, 7, 7, 8, 10, 15, 19, 25]
+    const accessories = [3, 4, 5, 6, 6, 7, 10, 13, 15, 25]
+
+    const combined = [...consumables, ...armor, ...weapons, ...accessories]
+    this.costs = uniq(combined).sort((a, b) => a > b)
+    this.maxCost = max(this.costs)
+    this.solutions = []
+    this.heldSolutions = []
+
+    this.getPossibleOptions(25, 2)
+    console.log(uniqWith(this.solutions, isEqual))
+    console.log(uniqWith(this.heldSolutions, isEqual))
+  }
+
+  getPossibleOptions = (total, remainingItems, partial = []) => {
+    const sum = partial.reduce((a, b) => a + b, 0)
+
+    if (sum > total) {
+      return
+    }
+
+    if (remainingItems === 0) {
+      if (sum === total) {
+        this.solutions.push(partial.sort((a, b) => a > b))
+      } else if (sum === total - 1) {
+        this.heldSolutions.push(partial.sort((a, b) => a > b))
+      } else {
+        return
+      }
+    }
+
+    if (total - sum > remainingItems * this.maxCost) {
+      return
+    }
+
+    for (let i = 0; i < this.costs.length; ++i) {
+      this.getPossibleOptions(total, remainingItems - 1, partial.concat(this.costs[i]))
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit
+            {' '}
+            <code>src/App.js</code>
+            {' '}
+and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
